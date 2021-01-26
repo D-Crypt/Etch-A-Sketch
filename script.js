d1c26de359c0278b0645fa306e60a1fd1df59866
D-Crypt <danielvaughan54@gmail.com>
2021-01-26 17:33:24 +0000

    Add setProperty for grid size

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ drawGrid(5);
 
 function drawGrid(size) {
     for (let i = 0; i < (size * size); i++) {
+        container.style.setProperty("--grid-size", size);
         let cell = document.createElement("div");
         cell.textContent = (i + 1);
         container.appendChild(cell);
