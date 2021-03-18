4716d6ba53c1b36beead3d636d2217f869bd5f11
D-Crypt <danielvaughan54@gmail.com>
2021-03-18 15:02:41 +0000

    Change grid items to squares

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function drawGrid(size) {
     for (let i = 0; i < (size * size); i++) {
         container.style.setProperty("--grid-size", size);
         let cell = document.createElement("div");
-        cell.textContent = (i + 1);
+        //cell.textContent = (i + 1);
         container.appendChild(cell);
     }
 }
